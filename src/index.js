d61e8a05dbfd0d55197184834986512c32af85a0
Didi <dignara@yandex.ru>
2021-03-10 18:00:29 +0300

    feat: add func logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (arguments.length === 0) {
+    return [];
+  }
+
+  return matrix.reduce((acc, el, i) => {
+    if (i % 2 === 0) {
+      acc.push(...el);
+    } else {
+      acc.push(...el.reverse());
+    }
+    return acc;
+  }, []);
+};
